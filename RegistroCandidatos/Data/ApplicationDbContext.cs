a334e4477ddd30b85d54eb6d4c1748d1815e480e
GuerreroRD <davidfermin2014@gmail.com>
2022-11-12 15:25:39 -0400

    Enviando Proyecto

---
+++ RegistroCandidatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RegistroCandidatos.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        //Insertar aqui los modelos para que puedan ser afectados por las migraciones.
+
+
+
+
+    }
+}
---
+++ RegistroCandidatos/Models/Candidato.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroCandidatos.Models
+{
+    public class Candidato
+    {
+        [Key, Required]
+        public int ID_Candidato { get; set; }
+
+        [Required]
+        [Display(Name = "Cedula")]
+        public string Cedula { get; set; }
+        [Required]
+        [Display(Name ="Nombres")]
+        public string Nombres { get; set; }
+
+        [Required]
+        [Display(Name = "Apellidos")]
+        public string Apellidos { get; set; }
+
+        [Display(Name ="Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime FechaDeNacimiento { get; set; }
+
+
+        [Display(Name ="Trabajo Actual")]
+        public string TrabajoActual{ get; set; }
+
+        [Display(Name ="Expectativa Salarial")]
+        public string ExpectativaSalarial { get; set; }
+    }
+}
